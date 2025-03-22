9f89539f13682d4f8d2b9ea409dd72569ae544bd
mohsen bashashi <45238123+mohsencherfl@users.noreply.github.com>
2025-03-22 15:38:29 +0330

    swap values using "pointers"
    This program demonstrates swapping the values of two integers using pointers. The swap function takes two integer pointers as arguments and exchanges their values by temporarily storing one value in a variable temp. In the main function, two integers (num1 and num2) are defined and displayed before swapping. The swap function is then called with the addresses of these integers, and the swapped values are displayed afterward. This implementation highlights the use of pointers to manipulate variables indirectly and perform value swapping.

---
+++ swap function/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *ptr1, int *ptr2);
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 20;
+
+    printf("Before swapping: \n");
+    printf("**num1** = %d \n", num1);
+    printf("**num2** = %d \n", num2);
+    printf("After swapping: \n");
+    swap(&num1, &num2);
+    printf("**num1** = %d \n", num1);
+    printf("**num2** = %d \n", num2);
+
+}
+
+void swap(int *ptr1, int *ptr2)
+{
+    int temp;
+    temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
