652b56f0435e991c79bde657336fe02112a85d90
mohsen bashashi <45238123+mohsencherfl@users.noreply.github.com>
2025-03-22 19:13:25 +0330

    swap values using "void pointer"
    This program implements a generic swap function, `genericSwap`, capable of exchanging the values of variables of any data type. The `genericSwap` function uses void pointers and the `size` parameter to dynamically allocate temporary memory, perform the swapping using `memcpy`, and free the memory afterward. In the `main` function, examples of swapping integer and double values are demonstrated. The program prints the values before and after the swap, showcasing the versatility of the generic swapping approach. This implementation highlights the use of void pointers and dynamic memory management for type-independent operations in C.

---
+++ swap using void pointer/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void genericSwap(void *a, void *b, int size);
+
+int main()
+{
+    int num1 = 10, num2 = 20;
+    double average1 = 70.45, average2 = 80.80;
+
+    printf("Before swapping: \n");
+    printf("Integer values: num1 = %d, num2 = %d \n", num1, num2);
+    printf("Integer values: average1 = %lf, average2 = %lf \n", average1, average2);
+    genericSwap(&num1, &num2, sizeof(int));
+    genericSwap(&average1, &average2, sizeof(double));
+    printf("After swapping: \n");
+    printf("Integer values: num1 = %d, num2 = %d \n", num1, num2);
+    printf("Integer values: average1 = %lf, average2 = %lf \n", average1, average2);
+
+    return 0;
+}
+
+void genericSwap(void *a, void *b, int size)
+{
+    void *tempMemory = malloc(size);
+
+    memcpy(tempMemory, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tempMemory, size);
+
+    free(tempMemory);
+}
+
