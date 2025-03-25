ac7b537f4a1214566834e4fe63155ca88fb10dc5
mohsen bashashi <45238123+mohsencherfl@users.noreply.github.com>
2025-03-25 22:19:27 +0330

    reverse an array of characters iteratively
    This program reverses the contents of a character array using an iterative approach. It swaps the characters in the array by progressively exchanging elements from opposite ends toward the center, utilizing a helper function swapCharacters. The program also includes a function, printArray, to display the array before and after the reversal. The main function initializes a character array, prints its original content, reverses the array using reverseArrayIterative, and then displays the reversed array.

---
+++ reverse character array/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SIZE 6
+
+void swapCharacters(char *ptr1, char *ptr2)
+{
+    char temp;
+    temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
+void printArray(char *arr, int size)
+{
+    for(int i=0; i<size; i++)
+        printf("%c", arr[i]);
+    printf("\n");
+}
+void reverseArrayIterative(char *arr, int size)
+{
+    for(int i=0; i<size/2; i++)
+    {
+        swapCharacters(&arr[i], &arr[size-1-i]);
+    }
+}
+
+int main()
+{
+    char name[SIZE] = {'m','o','h','s','e','n'};
+
+    printf("Before reversing: \n");
+    printArray(name, SIZE);
+    reverseArrayIterative(name, SIZE);
+    printf("After reversing: \n");
+    printArray(name, SIZE);
+
+    return 0;
+}
